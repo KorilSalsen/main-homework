d2492ce57fd7524e5cf6a7e7dde6cca5efed5500
Ilya Korchenov <ikorchenov@bk.ru>
2017-12-15 21:53:19 +0300

    edit deploy path

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
     -H "Authorization: Bearer $NETLIFY_KEY" \
     --data-binary "@build.zip" \
-    https://api.netlify.com/api/v1/sites/gracious-swanson-374aea
+    https://api.netlify.com/api/v1/sites
